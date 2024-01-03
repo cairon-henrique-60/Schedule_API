9fea568417cc8c051fad63cfd15c3bf0b7cae296
cairon-henrique-60 <caironhenrique60@gmail.com>
2024-01-03 20:27:52 -0300

    Feat. Protected Rotes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
 
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
 
 import { UserModule } from 'src/modules/users/user.module';
+import { AuthGuard } from './strategies/auth.guard';
 
 @Module({
   imports: [
@@ -18,7 +20,13 @@ import { UserModule } from 'src/modules/users/user.module';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
