f92af6ab92788c0ccf3955058ba9a27804892845
cairon-henrique-60 <caironhenrique60@gmail.com>
2024-01-06 12:43:49 -0300

    Fix. Resolved path entities

--- src/lib/database/database.providers.ts
+++ src/lib/database/database.providers.ts
@@ -1,6 +1,13 @@
 import { DataSource } from 'typeorm';
+import * as path from 'path';
 
 import { ENV_VARIABLES } from 'src/config/env.config';
+
+const entitiesPath = path.resolve(__dirname, '../../**/*.entity{.ts,.js}');
+const migrationsPath = path.resolve(
+  __dirname,
+  '../../lib/database/migrations/{*.ts,*.js}',
+);
 
 export const databaseProviders = [
   {
@@ -13,8 +20,8 @@ export const databaseProviders = [
         username: 'root',
         password: ENV_VARIABLES.DATABASE_ROOT_PASSWORD,
         database: ENV_VARIABLES.DATABASE_DATABASE_NAME,
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        migrations: [__dirname + '/**/migrations/{*.ts,*.js}'],
+        entities: [entitiesPath],
+        migrations: [migrationsPath],
         synchronize: false,
         migrationsRun: true,
         logging: true,
