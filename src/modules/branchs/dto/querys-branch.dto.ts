76aececf3fb4bc61c8ebf8811ab7fad060d23a7a
cairon-henrique-60 <caironhenrique60@gmail.com>
2024-01-27 06:49:33 -0300

    Feat. Improvements of the branch paginate

--- src/modules/branchs/dto/querys-branch.dto.ts
+++ src/modules/branchs/dto/querys-branch.dto.ts
@@ -12,6 +12,7 @@ const branchsSchema = z.object({
   local_number: z.string().max(10).optional(),
   branch_phone: z.string().optional().nullable(),
   complements: z.string().max(100).optional(),
+  user_id: z.string().optional(),
   createdAt: z.string().optional(),
   updatedAt: z.string().optional(),
   deletedAt: z.string().optional(),
@@ -73,7 +74,12 @@ export class QuerysBranchDto extends createZodDto(branchsSchema) {
    * @example Next to the bookstore
    */
   @ApiPropertyOptional()
-  complements?: string;
+  /**
+   * Owner of the branch.
+   * @example 355714ff-d013-4b14-ae1d-8e0694a453dc
+   */
+  @ApiPropertyOptional()
+  user_id?: string;
   /**
    *Date create of the user.
    *@example 2024-01-06
